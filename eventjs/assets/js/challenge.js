e265e9507ea9c00140036d03247e8942c074394b
Reggie <reggie.senal@gmail.com>
2024-05-28 22:37:29 +0800

    js challenge

--- eventjs/assets/js/challenge.js
+++ eventjs/assets/js/challenge.js
@@ -1,46 +1,15 @@
-let body = document.querySelector('body');
-let redBtn = document.querySelector('#redBtn');
-let yellowBtn = document.querySelector('#yellowBtn');
-let blueBtn = document.querySelector('#blueBtn');
-let greenBtn = document.querySelector('#greenBtn');
-let resetBtn = document.querySelector('#resetBtn');
-let resetColor = body.style.background;
+const body = document.querySelector('body');
+const btns = document.querySelectorAll('button');
+const resetColor = body.style.background;
 
 resetBtn.setAttribute('data-color', resetColor);
 
-redBtn.addEventListener('click', function (e) {
+btns.forEach(btn => btn.addEventListener('click', function (e) {
     e.preventDefault();
     setBackGroundColor(this);
-});
-
-yellowBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    setBackGroundColor(this);
-});
-
-blueBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    setBackGroundColor(this);
-});
-
-greenBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    setBackGroundColor(this);
-});
-
-resetBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    setBackGroundColor(this);
-});
+}));
 
 function setBackGroundColor(btn) {
-    redBtn.classList.remove('selected');
-    yellowBtn.classList.remove('selected');
-    blueBtn.classList.remove('selected');
-    greenBtn.classList.remove('selected');
-    resetBtn.classList.remove('selected');
-    btn.classList.add('selected');
     let c = btn.getAttribute('data-color');
     body.style.background = c;
-
 }
