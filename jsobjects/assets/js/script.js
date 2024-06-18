e5052ba578cff244352c5426340921773040304a
Reggie <reggie.senal@gmail.com>
2024-06-18 13:16:49 +0800

    boostrap and dynamic table with JS objects

--- jsobjects/assets/js/script.js
+++ jsobjects/assets/js/script.js
@@ -5,35 +5,71 @@ function calculateAge(bdate) {
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
 
+function fullname(fname, lname) {
+    return fname + " " + lname;
+}
+
+
+function generateColumn(data) {
+    const col = document.createElement("td");
+    col.innerText = data;
+    return col;
+}
+
+function generateRow(data) {
+    const trow = document.createElement("tr");
+    trow.appendChild(generateColumn(data.fname));
+    trow.appendChild(generateColumn(data.lname));
+    trow.appendChild(generateColumn(fullname(data.fname, data.lname)));
+    trow.appendChild(generateColumn(data.dateOfBirth));
+    trow.appendChild(generateColumn(calculateAge(data.dateOfBirth)));
+    return trow
+}
+
 const myData = [{
     fname: "John",
     lname: "Doe",
     dateOfBirth: "1980-01-01",
-    fullname() { return this.fname + " " + this.lname; },
-    age() { return calculateAge(this.dateOfBirth); }
 },
 {
     fname: "Sam",
     lname: "Lee",
     dateOfBirth: "1990-11-01",
-    fullname() { return this.fname + " " + this.lname; },
-    age() { return calculateAge(this.dateOfBirth); }
 },
 {
     fname: "Mike",
     lname: "Lake",
     dateOfBirth: "2000-09-11",
-    fullname() { return this.fname + " " + this.lname; },
-    age() { return calculateAge(this.dateOfBirth); }
+},
+{
+    fname: "Lars",
+    lname: "Tiago",
+    dateOfBirth: "1992-03-15",
+},
+{
+    fname: "Sally",
+    lname: "Fitz",
+    dateOfBirth: "1988-05-12",
+},
+{
+    fname: "Dom",
+    lname: "Nicks",
+    dateOfBirth: "1987-12-19",
 }];
 
-console.log(myData);
+
+/* console.log(myData);
 console.log(myData[0].fullname());
 console.log(myData[0].age());
-
 console.log(myData[1].fullname());
 console.log(myData[1].age());
-
-
 console.log(myData[2].fullname());
-console.log(myData[2].age());
+console.log(myData[2].age()); */
+
+const tble = document.querySelector("#tabledata");
+const tbleBody = document.createElement("tbody");
+myData.forEach((item, idx) => {
+    console.log(idx, item);
+    tbleBody.appendChild(generateRow(item));
+});
+tble.appendChild(tbleBody);
